3c614575eda02ba5ec812f66e0f7b125ca859db4
Avazbek Olimov <avazbekolimov722@gmail.com>
2026-09-05 21:40:57 +0300

    fix(host): preserve UTF-8 terminal bytes on Windows

--- Apps/TerminalHost/Program.cs
+++ Apps/TerminalHost/Program.cs
@@ -1,5 +1,6 @@
 using DevProjex.Terminal.CommandLine;
 using DevProjex.Terminal.Execution;
+using System.Text;
 
 namespace DevProjex.TerminalHost;
 
@@ -7,6 +8,9 @@ internal static class Program
 {
 	public static int Main(string[] args)
 	{
+		if (OperatingSystem.IsWindows())
+			ConfigureUtf8StandardStreams();
+
 		using var cancellation = TerminalCancellationCoordinator.Register();
 		return new TerminalApplication(
 				new InvocationEnvironment(hasAttachedConsole: true),
@@ -16,5 +20,15 @@ internal static class Program
 			.RunAsync(args, cancellation.Token)
 			.GetAwaiter()
 			.GetResult();
+	}
+
+	private static void ConfigureUtf8StandardStreams()
+	{
+		var utf8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+		Console.InputEncoding = utf8;
+		Console.OutputEncoding = utf8;
+		Console.SetIn(new StreamReader(Console.OpenStandardInput(), utf8, detectEncodingFromByteOrderMarks: true));
+		Console.SetOut(new StreamWriter(Console.OpenStandardOutput(), utf8) { AutoFlush = true });
+		Console.SetError(new StreamWriter(Console.OpenStandardError(), utf8) { AutoFlush = true });
 	}
 }
